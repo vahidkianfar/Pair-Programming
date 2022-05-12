6788b84f74e3b8f0ed89be661f5d3a60babf6c8e
vahidkianfar <vahidkianfar@gmail.com>
2022-05-12 20:15:31 +0100

    Cleaning.

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import numpy as np
 
-players = ["Adrian", "Aja", "Apshan", "Carl", "Eman", "Franco", "Hayley", "Ia", "Namita", "Nolan", "Sam",
+
+def AssignPlayersToTeams(players):
+
+    np.random.shuffle(players)
+    np.random.shuffle(players)
+    np.random.shuffle(players)
+    pairing = []
+
+    for _ in players:
+        pairing.append(players.pop(np.random.randint(0, len(players))))
+
+    [print(f'Pairing Team {i + 1} : ', players[i] + " and " + pairing[i]) for i in range(len(pairing))]
+
+
+members = ["Adrian", "Aja", "Apshan", "Carl", "Eman", "Franco", "Hayley", "Ia", "Namita", "Nolan", "Sam",
            "Shannon", "Sherry", "Vahid", "Vijay", "Vinu"]
 
-np.random.shuffle(players)
-np.random.shuffle(players)
-np.random.shuffle(players)
-
-pairing = []
-for player in players:
-    pairing.append(players.pop(np.random.randint(0, len(players))))
-
-for i in range(len(pairing)):
-    print(f'Pairing Team {i+1} : ', players[i] + " and " + pairing[i])
+AssignPlayersToTeams(members)
