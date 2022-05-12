c642cc6962b7e752addeeb51250e857a80c9e1e1
vahidkianfar <vahidkianfar@gmail.com>
2022-05-12 19:59:33 +0100

    some modification on for loops!

--- main.py
+++ main.py
@@ -11,5 +11,5 @@ pairing = []
 for player in players:
     pairing.append(players.pop(np.random.randint(0, len(players))))
 
-for i in range(len(players)):
+for i in range(len(pairing)):
     print(f'Pairing Team {i+1} : ', players[i] + " and " + pairing[i])
